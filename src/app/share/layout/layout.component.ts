8910ea4346c0d359af67cd7f2b8340e27efdce52
say-hai <mhlqian@qq.com>
2021-06-07 22:41:25 +0800

    feat:滑动隐藏或显示头部

--- src/app/share/layout/layout.component.ts
+++ src/app/share/layout/layout.component.ts
@@ -1,4 +1,16 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import {fromEvent, Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+import {HeaderComponent} from '../header/header.component';
 
 @Component({
   selector: 'app-layout',
@@ -7,12 +19,32 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   preserveWhitespaces: true
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit, AfterViewInit, OnDestroy {
+  private static VISIBLE_SCROLL_INSTANCE = 80;
+  showHeader = true;
 
-  constructor() {
+  @ViewChild(HeaderComponent, {read: ElementRef}) headerElementRef: ElementRef;
+  private destroy$ = new Subject<void>();
+
+  constructor(private hostElementRef: ElementRef,
+              private changeDetectorRef: ChangeDetectorRef) {
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    fromEvent(this.hostElementRef.nativeElement, 'scroll')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.showHeader = this.hostElementRef.nativeElement.scrollTop <= LayoutComponent.VISIBLE_SCROLL_INSTANCE;
+        this.changeDetectorRef.markForCheck();
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(null);
+    this.destroy$.complete();
   }
 
 }
