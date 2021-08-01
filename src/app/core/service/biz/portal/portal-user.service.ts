c6cb7c55cde6a5fca857b3e799674fb671021715
say-hai <mhlqian@qq.com>
2021-08-01 23:09:45 +0800

    feat:全局布局

--- src/app/core/service/biz/portal/portal-user.service.ts
+++ src/app/core/service/biz/portal/portal-user.service.ts
@@ -1,10 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpPlusClient} from 'ng-http-plus';
-import {ArticlePo} from '../../../model/po/article.po';
-import {JpaPageDto} from '../../../model/dto/jpa-page.dto';
-import {ArticleBriefDto} from '../../../model/dto/article-brief.dto';
-import {ArticleDetailDto} from '../../../model/dto/article-detail.dto';
 import {UserInfoDto} from '../../../../model/dto/user-info.dto';
 
 @Injectable({providedIn: 'root'})
@@ -17,23 +13,11 @@ export class PortalUserService {
       .get();
   }
 
-  updateUserInfo(userInfo:UserInfoDto): Observable<void> {
+  updateUserInfo(userInfo: UserInfoDto): Observable<void> {
     return HttpPlusClient.builder()
       .url(`/article-server/portal/users/info`)
       .body({})
       .post();
   }
 
-  findDetail(id: string): Observable<ArticleDetailDto> {
-    return HttpPlusClient.builder()
-      .url(`/article-server/articles/${id}`)
-      .get();
-  }
-
-  findBriefList(pageIndex = 0, pageSize = 20): Observable<ArticleBriefDto[]> {
-    return HttpPlusClient.builder()
-      .url(`/article-server/articles/recommend`)
-      .params({pageIndex: `${pageIndex}`, pageSize: `${pageSize}`})
-      .get();
-  }
 }
